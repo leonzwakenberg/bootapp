6e8a0709fa54fc3933087274f1492a4257c21d4a
leonzwakenberg <46625592+leonzwakenberg@users.noreply.github.com>
2019-02-13 16:15:31 +0100

    veel teovoegingen

--- src/main/java/com/capgemini/springboot/api/SchipEndPoint.java
+++ src/main/java/com/capgemini/springboot/api/SchipEndPoint.java
@@ -6,6 +6,7 @@ import com.capgemini.springboot.controller.SchipService;
 import com.capgemini.springboot.module.Schip;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,11 +17,12 @@ public class SchipEndPoint {
     private SchipRepository mijnSchipRepository;
         //mijnSchipRepository is de identifier
 
-    @GetMapping("/schip")
-    public Schip varen(){  // was eerst String, maar veranderd naar de Schip (entity) om alles terug te sturen
-        System.out.println("varen");
+    @GetMapping("/schip/{nieuwschip}")
+    public Schip nieuwSchip(@PathVariable String nieuwschip){
+  // was eerst String, maar veranderd naar de Schip (entity) om alles terug te sturen
+        System.out.println("nieuwschip");
         Schip hetSchip = new Schip();
-        hetSchip.naam = "Das Boot";
+        hetSchip.naam = nieuwschip;
         Schip vliegendeHollander = mijnSchipRepository.save(hetSchip);
        // Schip vliegendeHollander = mijnSchipRepository.save(new Schip());
         return vliegendeHollander;
